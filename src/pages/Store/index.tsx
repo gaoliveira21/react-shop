8260214d090e41ffc86046960e0ec145243e6f9f
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-12-19 20:43:11 -0300

    feat: add store header styles

--- src/pages/Store/index.tsx
+++ src/pages/Store/index.tsx
@@ -2,8 +2,19 @@ import React, { ReactElement } from 'react'
 
 import { Header } from '@/components'
 
+import css from './Store.sass'
+
 export const Store: React.FC = (): ReactElement => {
   return (
-    <Header />
+    <>
+      <Header />
+      <section className={css.Store}>
+        <h1 className={css.S__Title}>Produtos</h1>
+        <header className={css.S__Header}>
+          <strong className={css.H__TotalItens}>210 produtos encontrados</strong>
+          <div>dropdown</div>
+        </header>
+      </section>
+    </>
   )
 }
